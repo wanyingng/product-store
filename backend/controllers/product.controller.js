4a8bdee40f783db8e018b557e19ed3454b6885a4
xlepotato <yucia181@gmail.com>
2025-03-18 11:59:04 +0800

    refactor: clean backend code

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -51,11 +51,15 @@ export const updateProduct = async (req, res) => {
 export const deleteProduct = async (req, res) => {
     const { id } = req.params;
 
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "Product not found" });
+    }
+
     try {
         await Product.findByIdAndDelete(id);
         res.status(200).json({ success: true, message: "Product deleted successfully" });
     } catch (error) {
         console.error("Error in deleting product:", error.message);
-        res.status(404).json({ success: false, message: "Product not found" });
+        res.status(500).json({ success: false, message: "Server Error" });
     }
 };
--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,14 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // a middleware that allows us to accept JSON data in the req.body
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server is running on port 5000");
+    console.log("Server is running at http://localhost:" + PORT);
 });
