3a92f158afec20029f847ab74118462e761715fb
xlepotato <yucia181@gmail.com>
2025-03-19 14:20:10 +0800

    feat(frontend): add Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import { Button } from '@chakra-ui/react';
+import { Box, useColorModeValue } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <Button>Hello</Button>
-    </>
+    <Box minH={"100vh"} bg={useColorModeValue("pink.100", "gray.900")}>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/create" element={<CreatePage />} />
+        </Routes>
+    </Box>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </StrictMode>,
-)
+    <BrowserRouter>
+        <ChakraProvider>
+            <App />
+        </ChakraProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
