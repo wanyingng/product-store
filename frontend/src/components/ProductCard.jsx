2fbbb633a006468b61d6ba4275572bbea0e62615
xlepotato <yucia181@gmail.com>
2025-03-23 11:50:45 +0800

    feat(frontend): add support for product updates

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,13 +1,19 @@
 import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
-import { Box, Heading, HStack, IconButton, Image, Text, useColorModeValue, useToast } from "@chakra-ui/react";
+import { Box, Button, Heading, HStack, IconButton, Image, Input, Modal,
+         ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader,
+         ModalOverlay, Text, useColorModeValue, useDisclosure, useToast, VStack } from "@chakra-ui/react";
 import { useProductStore } from "../store/product";
+import { useState } from "react";
 
 const ProductCard = ({product}) => {
+    const [updatedProduct, setUpdatedProduct] = useState(product);
+
     const textColor = useColorModeValue("gray.600", "gray.200");
     const bg = useColorModeValue("white", "gray.800");
 
-    const { deleteProduct } = useProductStore();
+    const { deleteProduct, updateProduct } = useProductStore();
     const toast = useToast();
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     const handleDeleteProduct = async (productId) => {
         const { success, message } = await deleteProduct(productId);
@@ -23,6 +29,28 @@ const ProductCard = ({product}) => {
             toast({
                 title: "Success",
                 description: message,
+                status: "success",
+                duration: 3000,
+                isClosable: true
+            });
+        }
+    };
+
+    const handleUpdateProduct = async (productId, updatedProduct) => {
+        const { success, message } = await updateProduct(productId, updatedProduct);
+        onClose();
+        if (!success) {
+            toast({
+                title: "Error",
+                description: message,
+                status: "error",
+                duration: 3000,
+                isClosable: true
+            });
+        } else {
+            toast({
+                title: "Success",
+                description: "Product updated successfully",
                 status: "success",
                 duration: 3000,
                 isClosable: true
@@ -50,7 +78,7 @@ const ProductCard = ({product}) => {
 				</Text>
 
 				<HStack spacing={2}>
-					<IconButton icon={<EditIcon />} colorScheme="blue" />
+					<IconButton icon={<EditIcon />} onClick={onOpen} colorScheme="blue" />
 					<IconButton
 						icon={<DeleteIcon />}
                         onClick={() => handleDeleteProduct(product._id)}
@@ -58,8 +86,50 @@ const ProductCard = ({product}) => {
 					/>
 				</HStack>
             </Box>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Update Product</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <VStack spacing={4}>
+							<Input
+								placeholder="Product Name"
+								name="name"
+								value={updatedProduct.name}
+								onChange={(e) => setUpdatedProduct({ ...updatedProduct, name: e.target.value })}
+							/>
+							<Input
+								placeholder="Price"
+								name="price"
+								type="number"
+								value={updatedProduct.price}
+								onChange={(e) => setUpdatedProduct({ ...updatedProduct, price: e.target.value })}
+							/>
+							<Input
+								placeholder="Image URL"
+								name="image"
+								value={updatedProduct.image}
+								onChange={(e) => setUpdatedProduct({ ...updatedProduct, image: e.target.value })}
+							/>
+						</VStack>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button
+							colorScheme="pink"
+							mr={3}
+                            onClick={() => handleUpdateProduct(product._id, updatedProduct)}
+						>
+                            Update
+						</Button>
+						<Button variant="ghost" onClick={onClose}>
+							Cancel
+						</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </Box>
-  )
+  );
 };
 
 export default ProductCard;
--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -34,5 +34,23 @@ export const useProductStore = create((set) => ({
         // update the user interface immediately without having to refresh
         set(state => ({ products: state.products.filter(product => product._id !== pid) }));
         return { success: true, message: data.message };
+    },
+    updateProduct: async (pid, updatedProduct) => {
+        const res = await fetch(`/api/products/${pid}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(updatedProduct)
+        });
+        const data = await res.json();
+        if (!data.success) return { success: false, message: data.message };
+
+        // update the user interface immediately without having to refresh
+        set(state => ({
+            products: state.products.map((product) => (product._id === pid ? data.data : product))
+        }));
+
+        return { success: true, message: data.message };
     }
 }));
